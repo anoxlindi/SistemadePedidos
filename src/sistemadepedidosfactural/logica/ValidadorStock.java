305bc863214c8ff28216efb615f725374ff71c86
Analia <analiaaguerocorzo@gmail.com>
2025-11-04 23:26:38 -0500

    validaciónStock codificar

--- src/sistemadepedidosfactural/logica/ValidadorStock.java
+++ src/sistemadepedidosfactural/logica/ValidadorStock.java
@@ -3,5 +3,9 @@ package sistemadepedidosfactural.logica;
 
 
 public class ValidadorStock {
-    
+     public boolean validarStock(String producto, int cantidad) {
+        // Simulación: siempre hay stock si la cantidad es positiva
+        return cantidad > 0;
+    }
 }
+
