e1c0ae4855cfac73537403a5c7094ffce72e1ed7
Analia <analiaaguerocorzo@gmail.com>
2025-11-05 00:09:36 -0500

    adapte, usamos map y hashmap para obetner los datos de manera rápida y facil. es una estructura de datos

--- src/sistemadepedidosfactural/logica/FacturaAdapter.java
+++ src/sistemadepedidosfactural/logica/FacturaAdapter.java
@@ -1,6 +1,21 @@
 
 package sistemadepedidosfactural.logica;
-
-public class FacturaAdapter {
+import java.util.HashMap; // implementación del map, acceder facilmente a los datos
+import java.util.Map;// para agrupar datos de diferentes tipos es una sola estructura nombre:pepe, juana, tito, producto laptop, etc
+public class FacturaAdapter implements ServicioFactura{
     
+    private LegacyBillingSystem legacySystem = new LegacyBillingSystem();
+
+    @Override
+    public void generarFactura(String nombre, String producto, int cantidad, double subtotal, double igv, double total) {
+        Map<String, Object> datos = new HashMap<>();
+        datos.put("nombre", nombre);
+        datos.put("producto", producto);
+        datos.put("cantidad", cantidad);
+        datos.put("subtotal", subtotal);
+        datos.put("igv", igv);
+        datos.put("total", total);
+
+        legacySystem.emitirFacturaLegacy(datos);
+    }
 }
