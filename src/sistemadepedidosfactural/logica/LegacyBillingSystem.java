785e5fc82f4aa496f3c8d48a935852ea405e74e5
Analia <analiaaguerocorzo@gmail.com>
2025-11-05 00:04:05 -0500

    legacy billing system (sistema de facturación antiguo)

--- src/sistemadepedidosfactural/logica/LegacyBillingSystem.java
+++ src/sistemadepedidosfactural/logica/LegacyBillingSystem.java
@@ -1,6 +1,18 @@
 
 package sistemadepedidosfactural.logica;
 
+import java.util.Map;
+
 public class LegacyBillingSystem {
     
+    public void emitirFacturaLegacy(Map<String, Object> datos) {
+        System.out.println("=== FACTURA LEGACY ===");
+        System.out.println("Cliente: " + datos.get("nombre"));
+        System.out.println("Producto: " + datos.get("producto"));
+        System.out.println("Cantidad: " + datos.get("cantidad"));
+        System.out.println("Subtotal: S/ " + datos.get("subtotal"));
+        System.out.println("IGV: S/ " + datos.get("igv"));
+        System.out.println("Total: S/ " + datos.get("total"));
+        System.out.println("=======================");
+    }
 }
