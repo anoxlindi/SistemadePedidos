f0e92f801c6b03cc1dd3a6dc3aefe9a843d7b477
Analia <analiaaguerocorzo@gmail.com>
2025-11-04 23:55:23 -0500

    repositorio de pedidos

--- src/sistemadepedidosfactural/logica/RepositorioPedido.java
+++ src/sistemadepedidosfactural/logica/RepositorioPedido.java
@@ -2,5 +2,7 @@
 package sistemadepedidosfactural.logica;
 
 public class RepositorioPedido {
-    
+      public void registrar(String nombre, String producto, int cantidad) {
+        System.out.println("Pedido registrado: " + nombre + " - " + producto + " x" + cantidad);
+    }
 }
