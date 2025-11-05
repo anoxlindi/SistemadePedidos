395e913445aa11c0e58625cb3d7b2257a8555e3a
Analia <analiaaguerocorzo@gmail.com>
2025-11-04 23:53:23 -0500

    calcular el impuesto 18% de igv

--- src/sistemadepedidosfactural/logica/CalculadoraImpuestos.java
+++ src/sistemadepedidosfactural/logica/CalculadoraImpuestos.java
@@ -2,5 +2,16 @@
 package sistemadepedidosfactural.logica;
 
 public class CalculadoraImpuestos {
-    
+        public double calcularSubtotal(String producto, int cantidad) {
+        // Simulación: cada producto cuesta 10.0
+        return 10.0 * cantidad;
+    }
+
+    public double calcularIGV(double subtotal) {
+        return subtotal * 0.18;
+    }
+
+    public double calcularTotal(double subtotal, double igv) {
+        return subtotal + igv;
+    }
 }
