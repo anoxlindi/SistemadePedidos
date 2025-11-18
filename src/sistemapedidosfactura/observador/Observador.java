0c5feba7ef5c0b51f7f17423fc4b084cde753c68
Analia <analiaaguerocorzo@gmail.com>
2025-11-17 21:30:02 -0500

    codificamos observador

--- src/sistemapedidosfactura/observador/Observador.java
+++ src/sistemapedidosfactura/observador/Observador.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package sistemapedidosfactura.observador;
 
-/**
- *
- * @author Analia
- */
-public class Observador {
-    
+public interface Observador {
+    void actualizar (String evento);
 }
