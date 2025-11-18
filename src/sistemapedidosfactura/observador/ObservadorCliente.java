731d3fab61a6e48912abf5edf6be370cca02b5a4
Analia <analiaaguerocorzo@gmail.com>
2025-11-17 21:25:04 -0500

    añadimos observador e hilos, solo creamos las clases

---
+++ src/sistemapedidosfactura/observador/ObservadorCliente.java
@@ -0,0 +1,14 @@
+package sistemapedidosfactura.observador;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Analia
+ */
+public class ObservadorCliente {
+    
+}
